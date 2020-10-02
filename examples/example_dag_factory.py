cef9853928ce982be76d262b7a36ef03119e219a
Saket <52213128+saket04@users.noreply.github.com>
2020-10-02 19:02:11 +0530

    Update example_dag_factory.py (#60)
    * Update example_dag_factory.py
    
    * Update example_dag_factory.py
    
    Co-authored-by: Adam Boscarino <ajboscarino@gmail.com>

--- examples/example_dag_factory.py
+++ examples/example_dag_factory.py
@@ -4,5 +4,6 @@ import dagfactory
 config_file = "/usr/local/airflow/dags/example_dag_factory.yml"
 example_dag_factory = dagfactory.DagFactory(config_file)
 
+# Creating task dependencies
 example_dag_factory.clean_dags(globals())
 example_dag_factory.generate_dags(globals())
