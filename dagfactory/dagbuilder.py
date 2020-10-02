97c4c8b023993cb123a4942e5f8a9b6359bb92bc
Adam Boscarino <aboscarino@devoted.com>
2020-10-02 13:10:35 +0000

    fix linting errors

--- dagfactory/dagbuilder.py
+++ dagfactory/dagbuilder.py
@@ -46,7 +46,6 @@ class DagBuilder:
         in the YAML file
     """
 
-    # pylint: disable=bad-continuation
     def __init__(
         self, dag_name: str, dag_config: Dict[str, Any], default_config: Dict[str, Any]
     ) -> None:
@@ -65,7 +64,7 @@ class DagBuilder:
                 self.dag_config, self.default_config
             )
         except Exception as err:
-            raise Exception(f"Failed to merge config with default config, err: {err}")
+            raise "Failed to merge config with default config" from err
         dag_params["dag_id"]: str = self.dag_name
 
         # Convert from 'dagrun_timeout_sec: int' to 'dagrun_timeout: timedelta'
@@ -112,7 +111,7 @@ class DagBuilder:
                 timezone=dag_params["default_args"].get("timezone", "UTC"),
             )
         except KeyError as err:
-            raise Exception(f"{self.dag_name} config is missing start_date, err: {err}")
+            raise f"{self.dag_name} config is missing start_date" from err
         return dag_params
 
     @staticmethod
@@ -126,7 +125,7 @@ class DagBuilder:
             # class is a Callable https://stackoverflow.com/a/34578836/3679900
             operator_obj: Callable[..., BaseOperator] = import_string(operator)
         except Exception as err:
-            raise Exception(f"Failed to import operator: {operator}. err: {err}")
+            raise f"Failed to import operator: {operator}" from err
         try:
             if operator_obj == PythonOperator:
                 if not task_params.get("python_callable_name") and not task_params.get(
@@ -203,7 +202,7 @@ class DagBuilder:
 
             task: BaseOperator = operator_obj(**task_params)
         except Exception as err:
-            raise Exception(f"Failed to create {operator_obj} task. err: {err}")
+            raise f"Failed to create {operator_obj} task" from err
         return task
 
     def build(self) -> Dict[str, Union[str, DAG]]:
