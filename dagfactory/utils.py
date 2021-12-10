d0d10fbcb735bdfaccfc807c2b24b06ec7946a9a
Indy <indy.berthome@payfit.com>
2021-12-10 10:04:37 +0100

    Add environment variable support
    Add support for callable file path to use environment variable and not get error on get_python_callable function.

--- dagfactory/utils.py
+++ dagfactory/utils.py
@@ -118,6 +118,8 @@ def get_python_callable(python_callable_name, python_callable_file):
     :type: callable
     """
 
+    python_callable_file = os.path.expandvars(python_callable_file)
+
     if not os.path.isabs(python_callable_file):
         raise Exception("`python_callable_file` must be absolute path")
 
