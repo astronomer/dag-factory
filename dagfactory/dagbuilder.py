30688180f4a9385077ca45f9712e2881f72b4c04
Adam Boscarino <adam@bosco.codes>
2022-05-04 10:55:53 -0400

    Update dagfactory/dagbuilder.py

--- dagfactory/dagbuilder.py
+++ dagfactory/dagbuilder.py
@@ -201,6 +201,7 @@ class DagBuilder:
                     and not task_params.get("python_callable_name")
                     and not task_params.get("python_callable_file")
                 ):
+                    # pylint: disable=line-too-long
                     raise Exception(
                         "Failed to create task. PythonOperator, BranchPythonOperator and PythonSensor requires \
                         `python_callable_name` and `python_callable_file` "
