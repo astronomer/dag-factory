7e603f55f5e843ecec9ff3699bc6424f9eafb112
Adam Boscarino <ajboscarino@gmail.com>
2023-03-27 01:12:39 -0400

    revert kubernetes inheritance change (#158)
    * return slightly more meaningful exceptions
    
    * fmt
    
    * return slightly more meaningful exceptions
    
    * cleanup tests
    
    * revert broken change
    
    * remove 1.10.6 test

---
+++ dagfactory/exceptions.py
@@ -0,0 +1,13 @@
+"""Module contains exceptions for dag-factory"""
+
+
+class DagFactoryException(Exception):
+    """
+    Base class for all dag-factory errors.
+    """
+
+
+class DagFactoryConfigException(Exception):
+    """
+    Raise for dag-factory config errors.
+    """
