714ec77f93a5e7261c0f404af827e56ba7c703f0
Pankaj Koti <pankajkoti699@gmail.com>
2024-10-15 12:23:31 +0530

    Address @pankajastro's review comments

--- scripts/test/unit-cov.sh
+++ scripts/test/unit-cov.sh
@@ -4,5 +4,4 @@ pytest \
     --cov-report=term-missing \
     --cov-report=xml \
     --durations=0 \
-    -m "not (integration or perf)" \
     --ignore=tests/test_example_dags.py
--- scripts/test/unit.sh
+++ scripts/test/unit.sh
@@ -1,5 +1,4 @@
 pytest \
     -vv \
     --durations=0 \
-    -m "not (integration or perf)" \
     --ignore=tests/test_example_dags.py
