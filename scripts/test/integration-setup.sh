017bc30719021769e4b842c1c8fdd136fe657ced
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-12-09 14:40:00 +0000

    Improve integration tests scripts (#316)
    When running locally, they would generate a symbolic link inside the
    dags symbolic link, if it previously existed.
    
    This PR solves these limitations.

--- scripts/test/integration-setup.sh
+++ scripts/test/integration-setup.sh
@@ -4,6 +4,15 @@ set -v
 set -x
 set -e
 
+if [ -L "dags" ]; then
+    echo "Symbolic link 'dags' already exists."
+elif [ -e "dags" ]; then
+    echo "'dags' exists but is not a symbolic link. Please resolve this manually."
+else
+    ln -s dev/dags dags
+    echo "Symbolic link 'dags' created successfully."
+fi
+
 rm -rf airflow.*
 pip freeze | grep airflow
 airflow db reset -y
