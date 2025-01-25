14b1c193836c6b64ebb45f3bbf308de81e9428b3
pankajastro <pankaj.singh@astronomer.io>
2025-01-25 15:03:53 +0530

    Fix import path

--- dagfactory/__init__.py
+++ dagfactory/__init__.py
@@ -1,9 +1,10 @@
 """Modules and methods to export for easier access"""
 
-from .dagfactory import DagFactory, load_yaml_dags
+from .dagfactory import DagFactory, load_dags, load_yaml_dags
 
 __version__ = "0.22.0"
 __all__ = [
     "DagFactory",
     "load_yaml_dags",
+    "load_dags",
 ]
