408f5d43075a0d5796b86ff3b6ff3ed251d4820d
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-04-07 16:30:39 +0530

    Add missing Python file for dynamic task example (#392)
    The Python DAG file is missing for the configuration file
    example_dynamic_task_mapping.yml.
    
    This PR add it so that DAG will get trigger in CI

---
+++ dev/dags/example_dynamic_task_mapping.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+# The following import is here so Airflow parses this file
+# from airflow import DAG
+import dagfactory
+
+DEFAULT_CONFIG_ROOT_DIR = "/usr/local/airflow/dags/"
+
+CONFIG_ROOT_DIR = Path(os.getenv("CONFIG_ROOT_DIR", DEFAULT_CONFIG_ROOT_DIR))
+
+config_file = str(CONFIG_ROOT_DIR / "example_dynamic_task_mapping.yml")
+
+example_dag_factory = dagfactory.DagFactory(config_file)
+
+# Creating task dependencies
+example_dag_factory.clean_dags(globals())
+example_dag_factory.generate_dags(globals())
