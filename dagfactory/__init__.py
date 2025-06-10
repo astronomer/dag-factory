266f7f73b11ab04bf34661afc77b3da80d1806c3
Julian LaNeve <lanevejulian@gmail.com>
2025-06-10 16:33:51 -0400

    Use single-file format

--- dagfactory/__init__.py
+++ dagfactory/__init__.py
@@ -1,9 +1,12 @@
 """Modules and methods to export for easier access"""
 
 from .dagfactory import DagFactory, load_yaml_dags
+from .single_dag import load_single_dag, load_single_dag_directory
 
 __version__ = "0.23.0a4"
 __all__ = [
     "DagFactory",
     "load_yaml_dags",
+    "load_single_dag",
+    "load_single_dag_directory",
 ]
