ad91b8e6248d185076593f8c13a5659f63fd813a
pankajastro <pankaj.singh@astronomer.io>
2025-06-24 13:40:20 +0530

    Handle unknown tag

--- dagfactory/loader.py
+++ dagfactory/loader.py
@@ -1,3 +1,5 @@
+import importlib
+
 import yaml
 from kubernetes.client import models as k8s
 
@@ -61,6 +63,42 @@ class KubernetesConstructor:
         return k8s.V1Pod(**data)
 
 
+def _dynamic_object_constructor(loader, node):
+    tag = node.tag.lstrip("!")
+    parts = tag.split(".")
+    module_name = ".".join(parts[:-1])
+    class_name = parts[-1]
+
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError as e:
+        raise yaml.constructor.ConstructorError(f"Could not import module '{module_name}' for tag '{tag}': {e}")
+    try:
+        cls = getattr(module, class_name)
+    except AttributeError:
+        raise yaml.constructor.ConstructorError(
+            f"Class '{class_name}' not found in module '{module_name}' for tag '{tag}'"
+        )
+
+    if isinstance(node, yaml.MappingNode):
+        data = loader.construct_mapping(node, deep=True)
+    elif isinstance(node, yaml.SequenceNode):
+        data = loader.construct_sequence(node, deep=True)
+    elif isinstance(node, yaml.ScalarNode):
+        data = loader.construct_scalar(node)
+    else:
+        raise yaml.constructor.ConstructorError(f"Unsupported YAML node type for dynamic constructor: {type(node)}")
+
+    if isinstance(data, dict):
+        return cls(**data)
+    elif isinstance(data, list) and issubclass(cls, list):
+        return cls(data)
+    elif isinstance(data, str) and issubclass(cls, str):
+        return cls(data)
+    else:
+        return cls(data)
+
+
 class DAGFactoryLoader(yaml.FullLoader):
     pass
 
@@ -71,3 +109,5 @@ DAGFactoryLoader.add_constructor(
 DAGFactoryLoader.add_constructor("!kubernetes.client.models.V1Container", KubernetesConstructor.container_constructor)
 DAGFactoryLoader.add_constructor("!kubernetes.client.models.V1PodSpec", KubernetesConstructor.pod_spec_constructor)
 DAGFactoryLoader.add_constructor("!kubernetes.client.models.V1Pod", KubernetesConstructor.pod_constructor)
+# Handle unknown tag dynamically
+DAGFactoryLoader.add_constructor(None, _dynamic_object_constructor)
