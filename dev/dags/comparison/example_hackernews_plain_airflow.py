f5c74c5083b4dc093e9b18d82ca65265cca1972d
pankajastro <pankaj.singh@astronomer.io>
2025-07-05 00:05:04 +0530

    Disable some tests for testing

--- dev/dags/comparison/example_hackernews_plain_airflow.py
+++ dev/dags/comparison/example_hackernews_plain_airflow.py
@@ -1,37 +1,37 @@
-from datetime import datetime
-
-try:
-    from airflow.providers.standard.operators.bash import BashOperator
-    from airflow.providers.standard.operators.python import PythonOperator
-    from airflow.sdk.definitions.dag import DAG
-except ImportError:
-    from airflow.models.dag import DAG
-    from airflow.operators.bash import BashOperator
-    from airflow.operators.python import PythonOperator
-
-from hacker_news import summarize
-
-with DAG(dag_id="example_hackernews_plain_airflow", schedule=None, start_date=datetime(2022, 3, 4)) as dag:
-
-    fetch_top_ten_news = BashOperator(
-        task_id="fetch_top_ten_news",
-        bash_command="curl -s https://hacker-news.firebaseio.com/v0/topstories.json  | jq -c -r '.[0:10]'",
-    )
-
-    fetch_first_top_news = BashOperator(
-        task_id="fetch_first_top_news",
-        bash_command="""
-            echo {{ task_instance.xcom_pull(task_ids='fetch_top_ten_news') }} | jq -c -r '.[0]' |  xargs -I {} curl -s 'https://hacker-news.firebaseio.com/v0/item/{}.json'
-        """,
-    )
-
-    fetch_second_top_news = BashOperator(
-        task_id="fetch_second_news",
-        bash_command="""
-            echo {{ task_instance.xcom_pull(task_ids='fetch_top_ten_news') }} | jq -c -r '.[1]' |  xargs -I {} curl -s 'https://hacker-news.firebaseio.com/v0/item/{}.json'
-        """,
-    )
-
-    summarize = PythonOperator(task_id="summarize", python_callable=summarize)
-
-    fetch_top_ten_news >> [fetch_first_top_news, fetch_second_top_news] >> summarize
+# from datetime import datetime
+#
+# try:
+#     from airflow.providers.standard.operators.bash import BashOperator
+#     from airflow.providers.standard.operators.python import PythonOperator
+#     from airflow.sdk.definitions.dag import DAG
+# except ImportError:
+#     from airflow.models.dag import DAG
+#     from airflow.operators.bash import BashOperator
+#     from airflow.operators.python import PythonOperator
+#
+# from hacker_news import summarize
+#
+# with DAG(dag_id="example_hackernews_plain_airflow", schedule=None, start_date=datetime(2022, 3, 4)) as dag:
+#
+#     fetch_top_ten_news = BashOperator(
+#         task_id="fetch_top_ten_news",
+#         bash_command="curl -s https://hacker-news.firebaseio.com/v0/topstories.json  | jq -c -r '.[0:10]'",
+#     )
+#
+#     fetch_first_top_news = BashOperator(
+#         task_id="fetch_first_top_news",
+#         bash_command="""
+#             echo {{ task_instance.xcom_pull(task_ids='fetch_top_ten_news') }} | jq -c -r '.[0]' |  xargs -I {} curl -s 'https://hacker-news.firebaseio.com/v0/item/{}.json'
+#         """,
+#     )
+#
+#     fetch_second_top_news = BashOperator(
+#         task_id="fetch_second_news",
+#         bash_command="""
+#             echo {{ task_instance.xcom_pull(task_ids='fetch_top_ten_news') }} | jq -c -r '.[1]' |  xargs -I {} curl -s 'https://hacker-news.firebaseio.com/v0/item/{}.json'
+#         """,
+#     )
+#
+#     summarize = PythonOperator(task_id="summarize", python_callable=summarize)
+#
+#     fetch_top_ten_news >> [fetch_first_top_news, fetch_second_top_news] >> summarize
