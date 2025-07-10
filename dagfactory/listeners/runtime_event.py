c39879d80c71cc3f74c4bf4ee88b5a25611affa6
pankajastro <pankaj.singh@astronomer.io>
2025-07-10 18:14:58 +0530

    Revert some changes

--- dagfactory/listeners/runtime_event.py
+++ dagfactory/listeners/runtime_event.py
@@ -5,16 +5,10 @@ from airflow.listeners import hookimpl
 try:
     from airflow.sdk.definitions.dag import DAG
 except ImportError:
-    from airflow.models.dag import DAG
-import hashlib
-
+    from airflow.models import DAG
 from airflow.models.dagrun import DagRun
-from airflow.version import version as AIRFLOW_VERSION
-from packaging import version
 
 from dagfactory import telemetry
-
-INSTALLED_AIRFLOW_VERSION = version.parse(AIRFLOW_VERSION)
 
 
 class EventStatus:
@@ -36,15 +30,8 @@ def on_dag_run_success(dag_run: DagRun, msg: str):
     dag = dag_run.get_dag()
     if not is_dagfactory_dag(dag):
         return
-
-    if INSTALLED_AIRFLOW_VERSION < version.Version("3.0.0"):
-        dag_hash = dag_run.dag_hash
-    else:
-        dag_id_str = str(dag_run.dag_id)
-        dag_hash = hashlib.md5(dag_id_str.encode("utf-8")).hexdigest()
-
     additional_telemetry_metrics = {
-        "dag_hash": dag_hash,
+        "dag_hash": dag_run.dag_hash,
         "status": EventStatus.SUCCESS,
         "task_count": len(dag.task_ids),
     }
@@ -57,15 +44,8 @@ def on_dag_run_failed(dag_run: DagRun, msg: str):
     dag = dag_run.get_dag()
     if not is_dagfactory_dag(dag):
         return
-
-    if INSTALLED_AIRFLOW_VERSION < version.Version("3.0.0"):
-        dag_hash = dag_run.dag_hash
-    else:
-        dag_id_str = str(dag_run.dag_id)
-        dag_hash = hashlib.md5(dag_id_str.encode("utf-8")).hexdigest()
-
     additional_telemetry_metrics = {
-        "dag_hash": dag_hash,
+        "dag_hash": dag_run.dag_hash,
         "status": EventStatus.FAILED,
         "task_count": len(dag.task_ids),
     }
