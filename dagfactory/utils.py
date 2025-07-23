a7a8d682e9b421e74c78b4d871bb5e3f0a1782cc
pankajastro <pankaj.singh@astronomer.io>
2025-07-24 01:40:38 +0530

    Remove unused function

--- dagfactory/utils.py
+++ dagfactory/utils.py
@@ -8,9 +8,9 @@ import os
 import re
 import sys
 import types
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Any, AnyStr, Dict, List, Match, Optional, Pattern, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 
 import pendulum
 import yaml
@@ -26,68 +26,6 @@ def _import_from_string(class_path):
         return getattr(module, class_name)
     except (ImportError, AttributeError) as e:
         raise ImportError(f"Could not import '{class_path}': {e}")
-
-
-def get_datetime(date_value: Union[str, datetime, date], timezone: str = "UTC") -> datetime:
-    """
-    Takes value from DAG config and generates valid datetime. Defaults to
-    today, if not a valid date or relative time (1 hours, 1 days, etc.)
-
-    :param date_value: either a datetime (or date), a date string or a relative time as string
-    :type date_value: Uniont[datetime, date, str]
-    :param timezone: string value representing timezone for the DAG
-    :type timezone: str
-    :returns: datetime for date_value
-    :type: datetime.datetime
-    """
-    try:
-        local_tz: pendulum.timezone = pendulum.timezone(timezone)
-    except Exception as err:
-        raise DagFactoryException("Failed to create timezone") from err
-    if isinstance(date_value, datetime):
-        return date_value.replace(tzinfo=local_tz)
-    if isinstance(date_value, date):
-        return datetime.combine(date=date_value, time=datetime.min.time()).replace(tzinfo=local_tz)
-    # Try parsing as date string
-    try:
-        return pendulum.parse(date_value).replace(tzinfo=local_tz)
-    except pendulum.parsing.exceptions.ParserError:
-        # Try parsing as relative time string
-        rel_delta: timedelta = get_time_delta(date_value)
-        now: datetime = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0).replace(tzinfo=local_tz)
-        if not rel_delta:
-            return now
-        return now - rel_delta
-
-
-def get_time_delta(time_string: str) -> timedelta:
-    """
-    Takes a time string (1 hours, 10 days, etc.) and returns
-    a python timedelta object
-
-    :param time_string: the time value to convert to a timedelta
-    :type time_string: str
-    :returns: datetime.timedelta for relative time
-    :type datetime.timedelta
-    """
-    # pylint: disable=line-too-long
-    rel_time: Pattern = re.compile(
-        pattern=r"((?P<hours>\d+?)\s+hour)?((?P<minutes>\d+?)\s+minute)?((?P<seconds>\d+?)\s+second)?((?P<days>\d+?)\s+day)?",
-        # noqa
-        flags=re.IGNORECASE,
-    )
-    parts: Optional[Match[AnyStr]] = rel_time.match(string=time_string)
-    if not parts:
-        raise DagFactoryException(f"Invalid relative time: {time_string}")
-    # https://docs.python.org/3/library/re.html#re.Match.groupdict
-    parts: Dict[str, str] = parts.groupdict()
-    time_params = {}
-    if all(value is None for value in parts.values()):
-        raise DagFactoryException(f"Invalid relative time: {time_string}")
-    for time_unit, magnitude in parts.items():
-        if magnitude:
-            time_params[time_unit]: int = int(magnitude)
-    return timedelta(**time_params)
 
 
 def merge_configs(config: Dict[str, Any], default_config: Dict[str, Any]) -> Dict[str, Any]:
