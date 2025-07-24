2c54bb5bc4cb90781d2f045b624ee9bbca05312a
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-07-24 11:02:24 +0100

    Improve unittests so they disregard /Users/tatiana.alchueyr/Code/dag-factory/dev
    Previously, when setting the `$AIRFLOW_HOME` environment variable locally, as:
    $ export AIRFLOW_HOME=`pwd`/dev/
    
    Following our documentation to run integration tests and attempting to run:
    $ hatch run tests.py3.10-2.10:test-cov
    
    11 tests failed. When unsetting `$AIRFLOW_HOME`, the tests run successfully.
    
    This PR allows our unittests to run regardless of the system `$AIRFLOW_HOME`.

--- scripts/test/unit-cov.sh
+++ scripts/test/unit-cov.sh
@@ -1,4 +1,7 @@
-pytest \
+#!/bin/bash
+
+# Unset AIRFLOW_HOME to avoid using the local airflow installation
+env -u AIRFLOW_HOME pytest \
     -vv \
     --cov=dagfactory \
     --cov-report=term-missing \
