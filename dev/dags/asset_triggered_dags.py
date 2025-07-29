e2a46ba6a51a7371396743b1f04c9298b1693ca3
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-07-29 13:07:07 +0530

    Update dev/dags/asset_triggered_dags.py

--- dev/dags/asset_triggered_dags.py
+++ dev/dags/asset_triggered_dags.py
@@ -13,5 +13,4 @@ config_file = str(CONFIG_ROOT_DIR / "asset_triggered_dags.yml")
 example_dag_factory = dagfactory.DagFactory(config_file)
 
 # Creating task dependencies
-example_dag_factory.clean_dags(globals())
 example_dag_factory.generate_dags(globals())
