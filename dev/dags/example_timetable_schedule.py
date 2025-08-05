b0fbb44e4faf6f8c1a41123892922fb768f750b8
pankajastro <pankaj.singh@astronomer.io>
2025-08-05 20:02:28 +0530

    Fix tests

---
+++ dev/dags/example_timetable_schedule.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+# The following import is here so Airflow parses this file
+# from airflow import DAG
+from dagfactory import load_yaml_dags
+
+DEFAULT_CONFIG_ROOT_DIR = "/usr/local/airflow/dags/"
+CONFIG_ROOT_DIR = Path(os.getenv("CONFIG_ROOT_DIR", DEFAULT_CONFIG_ROOT_DIR))
+
+config_file = str(CONFIG_ROOT_DIR / "example_timetable_schedule.yml")
+
+load_yaml_dags(
+    globals_dict=globals(),
+    dags_folder=config_file,
+)
