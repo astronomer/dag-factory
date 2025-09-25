aa20353f44ac3d2ee2bf627cfcbc8542e10e41ee
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-09-25 15:15:45 +0530

    Update dagfactory/dagbuilder.py
    Co-authored-by: Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>

--- dagfactory/dagbuilder.py
+++ dagfactory/dagbuilder.py
@@ -141,7 +141,7 @@ class DagBuilder:
             "sla_miss_callback",  # Not applicable at the default_args level
         ]:
             if callback_type == "sla_miss_callback" and version.parse(AIRFLOW_VERSION) >= version.parse("3.1.0"):
-                # sla_miss_callbacks are removed as of 3.1.0. No need to raise a warning, we can silently skip
+                # sla_miss_callbacks are removed as of 3.1.0
                 logger.info("The sla_miss_callback has been removed in Airflow 3.1.0.")
                 continue
 
