1a55e1e56bc23c5b50f780aa932222292f212508
Pankaj Singh <ags.pankaj1996@gmail.com>
2026-05-05 16:03:26 +0530

    Fix dag

--- dev/dags/example_load_yaml_dags.py
+++ dev/dags/example_load_yaml_dags.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+# The following import is here so Airflow parses this file
+# from airflow import DAG
 from dagfactory import load_yaml_dags
 
 CONFIG_ROOT_DIR = Path(__file__).resolve().parent
